9d318aacafc044bfbfa520e0ec1452834114cd4f
grimalkin <loki@amarantha.net>
2019-11-12 16:46:45 +0100

    Second commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,54 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const ax = axios.create({
+    baseURL: 'http://localhost:5000/',
+    timeout: 5000,
+    headers: {'Authorization': 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjE1NzM2NTcwNDV9.4zYM_7ARGgbe7E2H5N2RcomNW85hOAXdjyFkd52zBls'}
+});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class ServiceList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { services: [], loading: true, error: false }
+    }
+    render() {
+        if ( this.state.loading ) {
+            return <p>(fetching services)</p>
+        } else if ( this.state.error ) {
+            return <p>Failed to load services!</p>
+        } else {
+            let services = []
+            this.state.services.forEach((service, i) => {
+                services.push(<Service key={i} data={service}/>);
+            });
+            return <ul>{services}</ul>
+        }
+    }
+    componentDidMount() {
+        ax.get("http://localhost:5000/services")
+            .then(res => {
+                this.setState({services: res.data, loading: false, error: false});
+            })
+            .catch(err => {
+                this.setState({services: [], loading: false, error: true})
+            });
+    }
+}
+
+class Service extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: props.data.id,
+            name: props.data.name
+        }
+    }
+    render() {
+        return <li key={this.state.id}><button>{this.state.name}</button></li>
+    }
+}
+
+
+ReactDOM.render(<ServiceList/>, document.getElementById('root'));
